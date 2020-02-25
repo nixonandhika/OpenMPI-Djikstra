014c9624d4dea3d01495d76e20b2348df66dd1a9
ChristzenLeonardy <christzenleonardy@gmail.com>
2020-02-25 16:02:31 +0700

    Refactor

--- dijsktra.c
+++ dijsktra.c
@@ -35,21 +35,19 @@ void dijkstra(int **graph, int src, int V, int *dist) {
 
         for (int v = 0; v < V; v++) {
             if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-            && dist[u] + graph[u][v] < dist[v]) {
+                && dist[u] + graph[u][v] < dist[v])
                 dist[v] = dist[u] + graph[u][v];
-            }
         }
     }
 }
 
 void fill_matrix(int** mat, int n) {
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if (i == j) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j)
                 mat[i][j] = 0;
-            } else {
+            else
                 mat[i][j] = rand() % 100;
-            }
         }
     }
 }
@@ -64,14 +62,17 @@ void write_file(int** mat, int n) {
         exit(1);
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
             fprintf(fptr, "%d ", mat[i][j]);
-        }
         fprintf(fptr, "\n");
     }
 
     fclose(fptr);
+}
+
+double get_duration_in_us(clock_t start_time, clock_t end_time) {
+    return ((double)(end_time - start_time) * 1000000 / CLOCKS_PER_SEC);
 }
 
 int main(int argc, char* argv[]) {
@@ -80,25 +81,27 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    //Read N from argument
+    /**
+     * Dictionary and Initialization
+     **/
+
+    // Read N from argument
     int n = atoi(argv[1]);
 
-    //Original matrix
-    int** mat = (int**) malloc (n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
-        mat[i] = (int*) malloc (n * sizeof(int));
-    }
+    // Original matrix
+    int** mat = malloc (n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+        mat[i] = malloc (n * sizeof(int));
 
-    //Result matrix
-    int** res_mat = (int**) malloc (n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
-        res_mat[i] = (int*) malloc (n * sizeof(int));
-    }
+    // Result matrix
+    int** res_mat = malloc (n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+        res_mat[i] = malloc (n * sizeof(int));
 
-    //Seed for random
+    // Seed for random
     srand(13517059);
 
-    //Fill matrix with random value
+    // Fill matrix with random value
     fill_matrix(mat, n);
 
     clock_t start_time = clock();
@@ -114,44 +117,51 @@ int main(int argc, char* argv[]) {
     int rank;
     MPI_Comm_rank(comm, &rank);
 
-    if (rank == 0) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                printf("%d ", mat[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
+    /**
+     * Algorithm
+     **/
+
+    // Print Sample Matrix
+    // if (rank == 0) {
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < n-1; j++) {
+    //             printf("%d ", mat[i][j]);
+    //         }
+    //         printf("%d\n", mat[i][n-1]);
+    //     }
+    //     printf("\n");
+    // }
 
     for (int i = rank; i < n; i += size) {
         dijkstra(mat, i, n, res_mat[i]);
-        if (rank != 0) {
+        if (rank != 0)
             MPI_Send(res_mat[i], n, MPI_INT, 0, 1, comm);
-        }
     }
 
     if (rank == 0) {
         for (int i = 1; i < n; i++) {
-            if (i % size != 0) {
+            if (i % size != 0)
                 MPI_Recv(res_mat[i], n, MPI_INT, i % size, 1, comm, &status);
-            }
         }
 
-        if (rank == 0) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    printf("%d ", res_mat[i][j]);
-                }
-                printf("\n ");
-            }
-        }
+        // Print Result Matrix
+        // if (rank == 0) {
+        //     for (int i = 0; i < n; i++) {
+        //         for (int j = 0; j < n-1; j++)
+        //             printf("%d ", res_mat[i][j]);
+        //         printf("%d\n", res_mat[i][n-1]);
+        //     }
+        // }
 
         clock_t end_time = clock();
-        printf("Finished in %f microseconds\n", ((double)(end_time - start_time) * 1000000 / CLOCKS_PER_SEC));
+        printf("Finished in %f microseconds\n", get_duration_in_us(start_time, end_time));
     }
 
     write_file(res_mat, n);
+
+    /**
+     * Destroy
+     **/
 
     for (int i = 0; i < n; i++) {
         free(mat[i]);
